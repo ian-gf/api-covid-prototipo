3a689b68bd828405c5bc74686f7ebf712c738f5b
ian-gf <iangsmite@gmail.com>
2021-03-09 15:57:49 -0800

    puntitos en las rutas

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -66,68 +66,68 @@ router.get('/api/registros', (req, res) => {
     res.send('Registros de API');
 });
 
-router.get('/api/registros/ciudad', (req, res) => {
+router.get('/api/registros/:ciudad', (req, res) => {
     res.send('Registros de API -> Ciudad');
 });
 
-router.get('/api/registros/nombre', (req, res) => {
+router.get('/api/registros/:nombre', (req, res) => {
     res.send('Registros de API -> Nombre');
 });
 
-router.get('/api/registros/sexo', (req, res) => {
+router.get('/api/registros/:sexo', (req, res) => {
     res.send('Registros de API -> Sexo');
 });
 
-router.get('/api/registros/edades', (req, res) => {
+router.get('/api/registros/:edades', (req, res) => {
     res.send('Registros de API -> Edades');
 });
 
-router.get('/api/registros/locacion', (req, res) => {
+router.get('/api/registros/:locacion', (req, res) => {
     res.send('Registros de API -> Locación');
 });
 
 //RUTAS FILTRADAS
-router.get('/api/registros/ciudad/nombre', (req, res) => {
+router.get('/api/registros/:ciudad/:nombre', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad y nombre');
 });
 
-router.get('/api/registros/ciudad/sexo', (req, res) => {
+router.get('/api/registros/:ciudad/:sexo', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad y sexo');
 });
 
-router.get('/api/registros/ciudad/edades', (req, res) => {
+router.get('/api/registros/:ciudad/:edades', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad y edades');
 });
 
-router.get('/api/registros/ciudad/locacion/nombre', (req, res) => {
+router.get('/api/registros/:ciudad/:locacion/:nombre', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad, locacion y nombre');
 });
 
-router.get('/api/registros/ciudad/locacion/edades', (req, res) => {
+router.get('/api/registros/:ciudad/:locacion/:edades', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad, locacion y edades');
 });
 
-router.get('/api/registros/ciudad/sexo/nombre', (req, res) => {
+router.get('/api/registros/:ciudad/:sexo/:nombre', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad, sexo y nombre');
 });
 
-router.get('/api/registros/ciudad/sexo/edades', (req, res) => {
+router.get('/api/registros/:ciudad/:sexo/:edades', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad, sexo y edades');
 });
 
-router.get('/api/registros/ciudad/sexo/edades/nombre', (req, res) => {
+router.get('/api/registros/:ciudad/:sexo/:edades/:nombre', (req, res) => {
     res.send('Registros de API -> Filtrada por ciudad, sexo, edades y nombre');
 });
 
-router.get('/api/registros/locacion/edades', (req, res) => {
+router.get('/api/registros/:locacion/:edades', (req, res) => {
     res.send('Registros de API -> Filtrada por locacion y edades');
 });
 
-router.get('/api/registros/locacion/sexo', (req, res) => {
+router.get('/api/registros/:locacion/:sexo', (req, res) => {
     res.send('Registros de API -> Filtrada por locacion y sexo');
 });
 
-router.get('/api/registros/locacion/nombre', (req, res) => {
+router.get('/api/registros/:locacion/:nombre', (req, res) => {
     res.send('Registros de API -> Filtrada por locacion y edades');
 });
 
