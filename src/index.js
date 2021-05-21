ea9bd8cd734db31fb150f851f5eb45964dd32d17
ian-gf <iangsmite@gmail.com>
2021-05-21 15:42:46 -0700

    commit -todoslosvideoslistos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 //Configuraciones
 app.set('port', 3000)
@@ -9,10 +10,10 @@ app.set('json spaces', 2);
 //middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 
 //rutas
 app.use(require('./routes/rutas'));
-app.use(require('./routes/admin'));
 
 //Empezando el servidor
 app.listen(app.get('port'));
